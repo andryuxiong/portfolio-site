7e1118f4cd902869d09f6e826979251f7e8b7d20
Andrew Xiong <andrewbarf123@gmail.com>
2025-05-14 21:51:55 -0500

    Configure Tailwind content paths and test utility

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1 className="text-4xl font-bold text-purple-500">Andrew Xiong</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
